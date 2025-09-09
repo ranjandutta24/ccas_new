4b80ea493181bbf262a68b5b05ef9e4e920f2da2
Ranjan Dutta <86516149+ranjandutta24@users.noreply.github.com>
2025-09-09 16:56:56 +0530

    work

--- src/app/main/compressors/compressors.component.ts
+++ src/app/main/compressors/compressors.component.ts
@@ -1,4 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexLegend,
+  ApexAxisChartSeries,
+  ApexGrid,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexXAxis,
+  ApexNonAxisChartSeries,
+  ApexFill,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  legend: ApexLegend;
+  colors: string[];
+};
+
+export type ChartROptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+};
 
 @Component({
   selector: 'app-compressors',
@@ -6,7 +38,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compressors.component.scss'],
 })
 export class CompressorsComponent implements OnInit {
+  @ViewChild('chart1') chart1!: ChartComponent;
+  @ViewChild('chart2') chart2!: ChartComponent;
+  @ViewChild('chart3') chart3!: ChartComponent;
+  @ViewChild('chart4') chart4!: ChartComponent;
+  @ViewChild('chart5') chart5!: ChartComponent;
+  @ViewChild('chart6') chart6!: ChartComponent;
+  @ViewChild('chart7') chart7!: ChartComponent;
+  @ViewChild('chart8') chart8!: ChartComponent;
   activeSection: string = 'section1';
+  public chartOptions1: ChartOptions;
+  public chartOptions2: ChartOptions;
+  public chartOptions3: ChartOptions;
+  public chartOptions4: ChartOptions;
+
+  // public chartROptions1: ChartOptions;
+  public chartROptions1: ChartROptions;
+  public chartROptions2: ChartROptions;
+  public chartROptions3: ChartROptions;
+  public chartROptions4: ChartROptions;
+
   setActiveSection(section: string): void {
     this.activeSection = section;
   }
@@ -14,7 +65,338 @@ export class CompressorsComponent implements OnInit {
   isActive(section: string): boolean {
     return this.activeSection === section;
   }
-  constructor() {}
+  constructor() {
+    const baseChartOptions = {
+      chart: {
+        height: 110,
+        // width: 300,
+        type: 'bar',
+        toolbar: {
+          show: true,
+          tools: {
+            download: false,
+            selection: true,
+            zoom: true,
+            zoomin: true,
+            zoomout: true,
+            pan: true,
+            reset: true,
+          },
+        },
+        animations: {
+          enabled: false,
+        },
+      } as ApexChart,
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+      },
+      colors: ['#208705'],
+      dataLabels: {
+        formatter: function (val: any, opts: any) {
+          const goals =
+            opts.w.config.series[opts.seriesIndex].data[opts.dataPointIndex]
+              .goals;
+
+          if (goals && goals.length) {
+            return `${val} / ${goals[0].value}`;
+          }
+          return val;
+        },
+      },
+      legend: {
+        show: true,
+        showForSingleSeries: true,
+        customLegendItems: ['Current', 'Limit'],
+        markers: {
+          fillColors: ['#208705', '#775DD0'],
+        },
+      },
+    };
+
+    this.chartOptions1 = {
+      ...baseChartOptions,
+      series: [
+        {
+          name: 'Actual',
+          data: [
+            {
+              x: 'Current Amp',
+              y: 124,
+              goals: [
+                {
+                  name: 'Expected',
+                  value: 600,
+                  strokeWidth: 5,
+                  strokeColor: '#775DD0',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    this.chartOptions2 = {
+      ...baseChartOptions,
+      series: [
+        {
+          name: 'Actual',
+          data: [
+            {
+              x: 'Current Amp',
+              y: 200,
+              goals: [
+                {
+                  name: 'Expected',
+                  value: 600,
+                  strokeWidth: 5,
+                  strokeColor: '#775DD0',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    this.chartOptions3 = {
+      ...baseChartOptions,
+      series: [
+        {
+          name: 'Actual',
+          data: [
+            {
+              x: 'Current Amp',
+              y: 124,
+              goals: [
+                {
+                  name: 'Expected',
+                  value: 134,
+                  strokeWidth: 5,
+                  strokeColor: '#775DD0',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    this.chartOptions4 = {
+      ...baseChartOptions,
+      series: [
+        {
+          name: 'Actual',
+          data: [
+            {
+              x: 'Current Amp',
+              y: 124,
+              goals: [
+                {
+                  name: 'Expected',
+                  value: 100,
+                  strokeWidth: 5,
+                  strokeColor: '#775DD0',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    this.chartROptions1 = {
+      series: [76],
+      chart: {
+        type: 'radialBar',
+        offsetY: -20,
+        // height: 500,
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          track: {
+            background: '#e7e7e7',
+            strokeWidth: '97%',
+            margin: 5, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: 2,
+              left: 0,
+              opacity: 0.31,
+              blur: 2,
+            },
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              offsetY: -2,
+              fontSize: '22px',
+            },
+          },
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          shadeIntensity: 0.4,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 53, 91],
+        },
+      },
+      labels: ['Average Results'],
+    };
+    this.chartROptions2 = {
+      series: [76],
+      chart: {
+        type: 'radialBar',
+        offsetY: -20,
+        // height: 500,
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          track: {
+            background: '#e7e7e7',
+            strokeWidth: '97%',
+            margin: 5, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: 2,
+              left: 0,
+              opacity: 0.31,
+              blur: 2,
+            },
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              offsetY: -2,
+              fontSize: '22px',
+            },
+          },
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          shadeIntensity: 0.4,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 53, 91],
+        },
+      },
+      labels: ['Average Results'],
+    };
+    this.chartROptions3 = {
+      series: [76],
+      chart: {
+        type: 'radialBar',
+        offsetY: -20,
+        // height: 500,
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          track: {
+            background: '#e7e7e7',
+            strokeWidth: '97%',
+            margin: 5, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: 2,
+              left: 0,
+              opacity: 0.31,
+              blur: 2,
+            },
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              offsetY: -2,
+              fontSize: '22px',
+            },
+          },
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          shadeIntensity: 0.4,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 53, 91],
+        },
+      },
+      labels: ['Average Results'],
+    };
+    this.chartROptions4 = {
+      series: [50],
+      chart: {
+        type: 'radialBar',
+        offsetY: -20,
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          // max: 500,
+          track: {
+            background: '#e7e7e7',
+            strokeWidth: '97%',
+            margin: 5,
+            dropShadow: {
+              enabled: true,
+              top: 2,
+              left: 0,
+              opacity: 0.31,
+              blur: 2,
+            },
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              offsetY: -2,
+              fontSize: '22px',
+              formatter: function (val: number) {
+                return '50'; // always show absolute value
+                // or: return val.toString(); if you want the actual series value
+              },
+            },
+          },
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          shadeIntensity: 0.4,
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 50, 53, 91],
+        },
+      },
+      labels: ['Average Results'],
+    };
+  }
 
   ngOnInit(): void {}
 }
